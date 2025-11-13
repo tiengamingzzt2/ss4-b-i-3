22530b0e2f6c3b2465a476dc83a96edaf8022c2e
tiengamingzzt2 <tiengamingzzt2@gmail.com>
2025-11-14 01:58:33 +0700

    Add files via upload

---
+++ ss4 bài 4.1.sql
@@ -0,0 +1,5 @@
+INSERT INTO companyDB (first_name, last_name, hiredate, salary) 
+VALUES 
+('nguyễn_thi', 'mai', '2023-01-15', 5000),
+('trang_', 'hoa', '2023-06-20', 6000),
+('Nguyễn_Thị_khánh', 'Linh', '20007-11-08', 9999);
